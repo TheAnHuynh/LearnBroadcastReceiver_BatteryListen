2295ffa09300e01af4f73be83361040e0b9a1e85
Huynh The An <flului1996@gmail.com>
2017-10-22 00:31:38 +0700

    comit

--- app/src/main/java/learnandroid/theanhuynh/learnbroadcastreceiver_batterylisten/MainActivity.java
+++ app/src/main/java/learnandroid/theanhuynh/learnbroadcastreceiver_batterylisten/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         btnThoat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                finish();
             }
         });
     }
